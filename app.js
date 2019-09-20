5446282257f066d3e9b7fc0ccc3847ebad39bcdd
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-09-20 19:26:22 +0530

    New Design

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let canvas = document.querySelector('canvas');
+
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height  = 0.9 * (window.innerHeight);
 
 let c = canvas.getContext('2d');
-c.fillStyle = "#ecf5ff";
-c.fillRect(0,0,innerWidth,innerHeight)
+
