ca4fc11dcd39c3f415922e0509a984a9f302d44f
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-09-21 13:12:10 +0530

    Egg flow at random Speed

--- app.js
+++ app.js
@@ -12,54 +12,78 @@ let initialPosition=50;
 let startPosition = initialPosition;
 let strokeHeight = (canvas.height) - 100;
 let basketPosition = 0;
-let dx=1;
-console.log(strokeHeight);
-animate();
+
+// animate();
 
 function start(){
-    createEgg1();
-    createEgg2();
-    createEgg3();
-    eggMotion();
+    requestAnimationFrame(start);
+    c.clearRect(0,0,canvas.width,canvas.height);
+    // eggMotion();
+
+    for(let i=0;i<eggs.length;i++){
+        eggs[i].draw();
+    }
     c.drawImage(img,(canvas.width*.25)-30,10,60,60);
     c.drawImage(img,(canvas.width*.5)-30,10,60,60);
     c.drawImage(img,(canvas.width*.75)-30,10,60,60);
-}
-function createEgg1(){
     
-    c.beginPath();
-    c.ellipse((canvas.width*.25),startPosition,5,7,0,0,2*Math.PI);
-    c.fillStyle='pink';
-    c.fill();
-    c.stroke();
-
 }
-function createEgg2(){
-    c.beginPath();
-    c.ellipse((canvas.width*.5),startPosition,5,7,0,0,2*Math.PI);
-    c.fillStyle='pink';
-    c.fill();
-    c.stroke();
 
-}
-function createEgg3(){
-    c.beginPath();
-    c.ellipse((canvas.width*.75),startPosition,5,7,0,0,2*Math.PI);
-    c.fillStyle='pink';
-    c.fill();
-    c.stroke();
+let y = startPosition;
+let majR = 5;
+let minR = 7;
+let angle= 0;
+let deg1 = 0;
+let deg2 = 2*Math.PI;
 
-}
-function eggMotion(){
-    if (startPosition<strokeHeight){
-        startPosition+=dx;
-        console.log(strokeHeight);
+let eggs =[];
+let egg1 = eggs.push(new CreateEgg((canvas.width*.25),y,majR,minR,angle,deg1,deg2));
+let egg2 = eggs.push(new CreateEgg((canvas.width*.5),y,majR,minR,angle,deg1,deg2));
+let egg3 = eggs.push(new CreateEgg((canvas.width*.75),y,majR,minR,angle,deg1,deg2));
+
+
+start();
+function CreateEgg(x,y,majR,minR,angle,deg1,deg2){
+    this.x=x;
+    this.y=y;
+    this.majorAngle=majR;
+    this.minorAngle=minR;
+    this.rotation=angle;
+    this.startAngle=deg1;
+    this.endAngle=deg2;
+    let dx =Math.floor((Math.random()*3)+1);
+
+    this.draw = function(){
+        c.beginPath();
+        c.ellipse(this.x,this.y,this.majorAngle,this.minorAngle,this.rotation,this.startAngle,this.endAngle);
+        c.fillStyle='pink';
+        c.fill();
+        c.stroke();
+        this.flow();
     }
+
+    this.flow = function(){
+        if(this.y<canvas.height-150){
+            this.y = this.y + dx;
+        }
+        else if(this.y===canvas.height-150){
+            this.y = startPosition;
+        }
+    }
+
+}
+// function eggMotion(){
+//     if (startPosition<strokeHeight){
+//         startPosition+=dx;
+//         console.log(strokeHeight);
+//     }
+// }
+function EggMotion(){
+
 }
 function animate(){
     requestAnimationFrame(animate);
-    c.clearRect(0,0,canvas.width,canvas.height);
     c.beginPath();
     c.moveTo(strokeHeight,basketPosition);
-    start();
+    
 }
